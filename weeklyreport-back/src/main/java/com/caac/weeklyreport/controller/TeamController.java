9146b050ebb0b8cb87884acbd30092b72210ced9
blackboyme <254832710@qq.com>
2025-06-22 01:44:48 +0800

    add: team

---
+++ weeklyreport-back/src/main/java/com/caac/weeklyreport/controller/TeamController.java
@@ -0,0 +1,40 @@
+package com.caac.weeklyreport.controller;
+
+
+import com.caac.weeklyreport.common.ResultBean;
+import com.caac.weeklyreport.entity.Team;
+import com.caac.weeklyreport.entity.User;
+import com.caac.weeklyreport.service.ITeamService;
+import com.caac.weeklyreport.service.impl.TeamServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 团队部门表 前端控制器
+ * </p>
+ *
+ * @author hanrenjie
+ * @since 2025-06-22
+ */
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+
+    private final ITeamService teamServiceImpl;
+
+    public TeamController(TeamServiceImpl teamServiceImpl) {
+        this.teamServiceImpl = teamServiceImpl;
+    }
+
+    @GetMapping("/getAllTeams")
+    public ResultBean<?> getAllTeams() {
+        List<Team> teams = teamServiceImpl.getAllTeams();
+        return ResultBean.success(teams);
+    }
+}
